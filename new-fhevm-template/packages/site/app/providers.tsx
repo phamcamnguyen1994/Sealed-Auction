f8f0cff68665f9271b3391559cd2b02ecccd0125
phamcamnguyen1994 <phamcamnguyen1994@gmail.com>
2025-09-12 14:50:30 +0700

    khi nào hết lỗi 404

--- new-fhevm-template/packages/site/app/providers.tsx
+++ new-fhevm-template/packages/site/app/providers.tsx
@@ -5,7 +5,6 @@ import type { ReactNode } from "react";
 import { MetaMaskProvider } from "@/hooks/metamask/useMetaMaskProvider";
 import { InMemoryStorageProvider } from "@/hooks/useInMemoryStorage";
 import { MetaMaskEthersSignerProvider } from "@/hooks/metamask/useMetaMaskEthersSigner";
-import { ThemeProvider } from "@/contexts/ThemeContext";
 
 type Props = {
   children: ReactNode;
@@ -13,12 +12,10 @@ type Props = {
 
 export function Providers({ children }: Props) {
   return (
-    <ThemeProvider>
-      <MetaMaskProvider>
-        <MetaMaskEthersSignerProvider initialMockChains={{ 31337: "http://localhost:8545" }}>
-          <InMemoryStorageProvider>{children}</InMemoryStorageProvider>
-        </MetaMaskEthersSignerProvider>
-      </MetaMaskProvider>
-    </ThemeProvider>
+    <MetaMaskProvider>
+      <MetaMaskEthersSignerProvider initialMockChains={{ 31337: "http://localhost:8545" }}>
+        <InMemoryStorageProvider>{children}</InMemoryStorageProvider>
+      </MetaMaskEthersSignerProvider>
+    </MetaMaskProvider>
   );
 }
